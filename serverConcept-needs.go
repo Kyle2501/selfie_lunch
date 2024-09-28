e902bf34abc939c00b83ea065a17b63574d73456
kyle2501@gmail.com <kyle2501@gmail.com>
2024-09-27 18:51:01 -0700

    update serverConcept-needs.go

---
+++ serverConcept-needs.go
@@ -0,0 +1,23 @@
+<div class="concept_needs_list">
+	<p><b>_ Concept Needs List A</b><ul>
+		<li>_ Equiptment</li>
+		<li>_ Employees</li>
+		<li>_ Ingredients </li>
+		<li>_ Process</li>
+		<li>_ Menu</li>
+		<li>_ Vehicle</li>
+		<li>_ Software</li>
+		<li>_ Brand</li>
+		<li>_ Sales</li>
+	</ul></p>
+</div><!-- - . concept_needs_list - -->
+	
+<div class="concept_needs_list">
+  <p><b>_ Concept Needs List B</b><ul>
+   <li>_ People</li>
+  <li>_ Booth</li>
+  <li>_ Product</li>
+    <li>_ Brand</li>
+    <li>_ Software</li>
+<li>_ Sales</li>	
+   </ul></p>
