06588cc4efbabb79658686c33a7be4f8961fe309
kyle2501@gmail.com <kyle2501@gmail.com>
2024-10-12 16:43:28 -0700

    update main.go

--- main.go
+++ main.go
@@ -587,7 +587,7 @@ main { background-color: #fff; margin-top: 85px; }
 
     
     closePath := `function closePath() {
-    document.getElementById("pagePath").innerHTML = " - <div class='pathButtons'><button onclick='openPath()'>Page Path</button></div>"
+    document.getElementById("pagePath").innerHTML = " - <div class='pathButtons'><button onclick='showPath()'>Page Path</button></div>"
 }`
   openPath := fmt.Sprintf("function showPath() { document.getElementById('pagePath').innerHTML = 'hii hi'; }")
      
